2a032e6307a8f56d39580ca3377c3d196e2fb99d
18jelias <jacquie19_e@yahoo.com>
2016-03-28 04:48:05 -0500

    Cleaned up code

--- Project4/song.rb
+++ Project4/song.rb
@@ -25,7 +25,6 @@ STATE_ALBUM = "3"
 STATE_ARTIST = "4"
 STATE_SONG = "5"
 
-state = 0
 input = 0;
 
 #Displays menu
@@ -42,22 +41,27 @@ end
 #Displays song, genre, and album
 def display_all
 	db = SQLite3::Database.new(DB_FILE_NAME)
+
 	db.execute(SQL_SELECT_SONGS) do |row| #iterator 
 		puts "Song: " + "#{row[0]}" + " Genre: " + "#{row[1]}" + " Album: " + "#{row[2]}"
 		puts "\n"
 	end
+	
 	db.close	
 end
 
 #Displays genre and inserts new genre
 def display_genres
 	db = SQLite3::Database.new(DB_FILE_NAME)
+
 	db.execute(SQL_SELECT_GENRES) do |row| #iterator 
 		puts row
 	end
 	puts "\n"
+
 	puts "New genre name:"
 	new_name = gets.chomp
+
 	db.execute "INSERT INTO genres(name) VALUES('#{new_name}');"
 	db.close
 end
@@ -65,16 +69,19 @@ end
 #Displays artist, albums and then inserts new album
 def display_album
 	db = SQLite3::Database.new(DB_FILE_NAME)
+
 	db.execute(SQL_SELECT_ARTISTS) do |row| #iterator 
 		puts row
 	end
 	puts "\n"	
+
 	puts "Select album artist:"
 	new_artist = gets.chomp
 	db.execute("SELECT name FROM albums") do |row| #iterator 
 		puts row
 	end
 	puts "\n"	
+
 	puts "New album name:"
 	new_album = gets.chomp
 
@@ -85,12 +92,15 @@ end
 #Displays artists then adds new artist
 def display_artist
 	db = SQLite3::Database.new(DB_FILE_NAME)
+
 	db.execute(SQL_SELECT_ARTISTS) do |row| #iterator 
 		puts row
 	end
 	puts "\n"	
+
 	puts "New artist name:"
 	new_artist = gets.chomp
+
 	db.execute "INSERT INTO artists(name) VALUES('#{new_artist}');"	
 	db.close
 end
@@ -98,39 +108,42 @@ end
 #Displays genre and album and then inserts a new song
 def display_song
 	db = SQLite3::Database.new(DB_FILE_NAME)
+
 	puts "Add a new song!"
 	new_song = gets.chomp
 	db.execute(SQL_SELECT_GENRES) do |row| #iterator 
 		puts row
 	end
 	puts "\n"	
+
 	puts "Choose the genre of the song:"
 	new_genre = gets.chomp	
 	db.execute("SELECT name FROM albums") do |row| #iterator 
 		puts row
 	end
 	puts "\n"	
+
 	puts "Choose the album of the song:"
 	new_album = gets.chomp
+
 	db.execute "INSERT INTO songs(name, genre_id, album_id) VALUES('#{new_song}', #{new_genre}', '#{new_album}');"
 	db.close
 end
 
 #Main Loop
-
 go = true;
 while go do
 	display_menu;
 	input = gets.chomp
-	if input == "1"
+	if input == STATE_ALLSONGS
 		display_all;
-	elsif input == "2"
+	elsif input == STATE_GENRE
 		display_genres;
-	elsif input == "3"
+	elsif input == STATE_ALBUM
 		display_album;
-	elsif input == "4"
+	elsif input == STATE_ARTIST
 		display_artist;
-	elsif input == "5"
+	elsif input == STATE_SONG
 		display_song;
 	end
 end
